a94d015ede7c7c67873ee78e7789c49678cebd10
ubuntu20 <tommy20051987@outlook.com>
2022-03-02 23:20:28 +0800

    fix

--- chap1/stdio-fopn/fopn.c
+++ chap1/stdio-fopn/fopn.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <string.h>
 int main(){
     FILE *fp;
     fp = fopen("tmp","r");
     if (fp == NULL){
-        fprintf(stderr,"fopen(): failed,errno = %d\n",errno);
+        //fprintf(stderr,"fopen(): failed,errno = %d\n",errno);
+        perror("fopen()");
+        fprintf(stderr,"fopen() error : %s \n",strerror(errno));
         exit(1);
     }
     puts("ok");

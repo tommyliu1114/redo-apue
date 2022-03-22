7bce86fee3a6c64269f8c7f65a8591ad35466964
lewis <tommy2005197@outlook.com>
2022-03-22 15:23:31 +0800

    fix

---
+++ chap7/chap7-adv-noblock/main.c
@@ -0,0 +1,121 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h> 
+#include <unistd.h>
+#include <errno.h>
+#define TTY1 "/dev/tty11"
+#define TTY2 "/dev/tty12"
+
+enum {
+    STATE_R = 1,
+    STATE_W,
+    STATE_EX,
+    STATE_T 
+};
+
+struct fsm_st {
+    int state; //当前状态机状态
+    int sfd;
+    int dfd;
+    char buff[BUFSIZ];
+    int len ;
+    int pos ;
+    char *errstr;
+};
+
+static void fsm_driver(struct fsm_st *fsm){
+    switch (fsm->state)
+    {
+    case STATE_R:
+        fsm->len = read(fsm->sfd,fsm->buff,BUFSIZ);
+        if (fsm->len == 0){
+            fsm->state = STATE_T;
+        }else if (fsm->len < 0){
+            if (errno == EAGAIN){
+                fsm->state = STATE_R;
+            }else {
+                fsm->errstr = "read()";
+                fsm->state = STATE_EX;
+            }
+        }else{
+            fsm->state = STATE_W;
+            fsm->pos = 0;
+
+        }
+        break;
+    case STATE_W:
+        int ret = write(fsm->dfd,fsm->buff+fsm->pos,fsm->len);
+        if (ret < 0){
+            if (errno == EAGAIN){
+                fsm->state = STATE_W;
+            }else{
+                fsm->errstr = "write()";
+                fsm->state = STATE_EX;
+            }
+        }else{
+            fsm->pos += ret;
+            fsm->len -= ret;
+            if (fsm->len == 0){
+                fsm->state = STATE_R;
+            }else{
+                fsm->state = STATE_W;
+            }
+        }
+        break;
+    case STATE_EX:
+        perror(fsm->errstr);
+        fsm->state = STATE_T;
+        break;
+    case STATE_T:
+        break;
+    default:
+        break;
+    }
+}
+
+static void relay(int fd1,int fd2){
+    struct fsm_st fsm12; //
+    struct fsm_st fsm21; //
+    int fd1_save;
+    fd1_save = fcntl(fd1,F_GETFL);
+    fcntl(fd1,F_SETFL,fd1_save | O_NONBLOCK);
+    int fd2_save;
+    fd2_save = fcntl(fd2,F_GETFL);
+    fcntl(fd2,F_SETFL,fd2_save | O_NONBLOCK);
+    fsm12.state = STATE_R;
+    fsm12.sfd = fd1;
+    fsm12.dfd = fd2;
+    
+    fsm21.state = STATE_R;
+    fsm21.sfd = fd2;
+    fsm21.dfd = fd1;
+    //
+    while(fsm12.state != STATE_T || fsm21.state != STATE_T){
+        fsm_driver(&fsm12);
+        fsm_driver(&fsm21);
+    }
+
+    fcntl(fd1,F_SETFL,fd1_save);
+    fcntl(fd2,F_SETFL,fd2_save);
+    }
+int main(){
+    int fd1,fd2;
+    fd1 = open(TTY1,O_RDWR);
+    if (fd1< 0){
+        perror("open():");
+        exit(1);
+    }
+    write(fd1,"tty1\n",5);
+    fd2 = open(TTY1,O_RDWR | O_NONBLOCK);
+    if (fd2< 0){
+        perror("open():");
+        exit(1);
+    }
+    write(fd1,"tty2\n",5);
+    relay(fd1,fd2);
+    close(fd1);
+    close(fd2);
+    exit(0);
+}
---
+++ ubuntu-flash/readme.md
@@ -0,0 +1,6 @@
+# 命令
+sudo apt install flashplugin-installer
+sudo apt install browser-plugin-freshplayer-pepperflash
+
+# ubuntu soft center 
+安装ffmpeg
